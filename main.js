77dd126fa2e2690ff2af49cb7d31bacb653ff96f
LuBrizuela <105133391+LuBrizuela@users.noreply.github.com>
2024-01-22 19:11:29 -0300

    correccion de errores
    corrección de errores de la api y diseño del footer.

--- main.js
+++ main.js
@@ -3,46 +3,70 @@ var navbar = document.querySelector('.navbar');
 var logo = document.querySelector('.navbar-logo');
 var sticky = navbar.offsetTop;
 
-// Añade la clase 'fixed-top' y muestra el logo al hacer scroll
+
 function stickNavbar() {
   if (window.scrollY >= sticky) {
     navbar.classList.add('fixed-top');
-    logo.style.display = 'block'; // Muestra el logo
+    logo.style.display = 'block';
     document.body.style.paddingTop = navbar.offsetHeight + 'px';
   } else {
     navbar.classList.remove('fixed-top');
-    logo.style.display = 'none'; // Oculta el logo
+    logo.style.display = 'none';
     document.body.style.paddingTop = '0';
   }
 }
 
-// Escucha el evento de scroll
+
 window.onscroll = function () {
   stickNavbar();
 };
 
+// carrusel
 
-function createProductsCards(products) {
-  const container = document.getElementById("producto-container");
-
-  products.forEach(product => {
-    const card = document.createElement("div");
-    card.classList.add("card", "mb-3");
-
-    const imageUrl = product.images[0].src;
-    const name = product.name.es;
-    const price = product.variants[0].price
-
-    card.innerHTML = `
-            <img src="${imageUrl} class="card-img-top" alt="${name}">
-            <div class="card-body">
-              <h5 class="card-title">${name}</h5>
-              <p class="card-text">$${price}</p>
-            </div>
-            
-    `;
-
-    container.appendChild(card);
+function filterProductsByCategory(products, categoryName) {
+  return products.filter(product => {
+    return product.categories && product.categories.some(category => category.name.es === categoryName);
   });
 }
 
+
+function createProductsCards(allProducts) {
+  const products = filterProductsByCategory(allProducts, "promocion");
+  const carouselInner = document.getElementById("carousel-inner");
+  carouselInner.innerHTML = '';
+
+
+  for (let i = 0; i < products.length; i += 4) {
+    const items = products.slice(i, i + 4);
+
+    const carouselItem = document.createElement("div");
+    carouselItem.classList.add("carousel-item");
+    if (i === 0) carouselItem.classList.add("active"); 
+
+    const cardsWrapper = document.createElement("div");
+    cardsWrapper.classList.add("cards-wrapper");
+
+
+    items.forEach(product => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      const imageUrl = product.images[0].src;
+      const name = product.name.es;
+      const price = product.variants[0].price;
+
+      card.innerHTML = `
+        <img src="${imageUrl}" class="card-img-top" alt="${name}">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text">$${price}</p>
+        </div>
+      `;
+
+      cardsWrapper.appendChild(card);
+    });
+
+    carouselItem.appendChild(cardsWrapper);
+    carouselInner.appendChild(carouselItem);
+  }
+};
